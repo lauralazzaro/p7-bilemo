bb2644708168750efb4c2a59619258fe8d64f9dc
lauralazzaro <lz_laura@hotmail.com>
2022-10-12 19:45:09 +0200

    Issue #8 #9 : routes for create product and delete product

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,17 +3,21 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Repository\BrandRepository;
 use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductController extends AbstractController
 {
-    #[Route('/api/product', name: 'app_product')]
+    #[Route('/api/products', name: 'app_product', methods: ['GET'])]
     public function index(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
     {
         $productList = $productRepository->findAll();
@@ -24,7 +28,7 @@ class ProductController extends AbstractController
         return new JsonResponse($jsonProductList, Response::HTTP_OK, [], true);
     }
 
-    #[Route('/api/product/{id}', name: 'app_product_detail')]
+    #[Route('/api/products/product/{id}', name: 'app_product_detail', methods: ['GET'])]
     public function details(ProductRepository $productRepository, SerializerInterface $serializer, int $id): JsonResponse
     {
         $productDetail = $productRepository->find($id);
@@ -36,5 +40,37 @@ class ProductController extends AbstractController
         }
 
         return new JsonResponse(null, Response::HTTP_NOT_FOUND);
+    }
+
+    #[Route('/api/products', name:"createProduct", methods: ['POST'])]
+    public function createProduct(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator, BrandRepository $brandRepository): JsonResponse
+    {
+        $product = $serializer->deserialize($request->getContent(), Product::class, 'json');
+
+        $content = $request->toArray();
+
+        $idBrand = $content['brandId'] ?? -1;
+
+        $product->setBrand($brandRepository->find($idBrand));
+
+        $em->persist($product);
+        $em->flush();
+
+        $context = SerializationContext::create()->setGroups(['getProducts']);
+        $jsonProduct = $serializer->serialize($product, 'json', $context);
+
+        $location = $urlGenerator->generate('app_product_detail', ['id' => $product->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
+
+        return new JsonResponse($jsonProduct, Response::HTTP_CREATED, ["Location" => $location], true);
+    }
+
+
+    #[Route('/api/products/product/{id}/delete', name: 'app_product_delete', methods: ['DELETE'])]
+    public function deleteProduct(Product $product, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($product);
+        $em->flush();
+
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
     }
 }
