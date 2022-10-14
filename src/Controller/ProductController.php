953877a6ba9da6091bf1cb643e616fb173004238
lauralazzaro <lz_laura@hotmail.com>
2022-10-14 13:46:59 +0200

    Issue #20 : hateoas for products and users

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,19 +2,13 @@
 
 namespace App\Controller;
 
-use App\Entity\Product;
-use App\Repository\BrandRepository;
 use App\Repository\ProductRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class ProductController extends AbstractController
 {
