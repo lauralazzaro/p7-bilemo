0b7d97007051ad06bf58ce77571b92fbb26634cf
lauralazzaro <lz_laura@hotmail.com>
2022-10-21 09:37:50 +0200

    Issue #27 : pagination for list of products

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -7,15 +7,20 @@ use JMS\Serializer\SerializationContext;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ProductController extends AbstractController
 {
     #[Route('/api/products', name: 'app_product', methods: ['GET'])]
-    public function index(ProductRepository $productRepository, SerializerInterface $serializer): JsonResponse
+    public function index(ProductRepository $productRepository, SerializerInterface $serializer, Request $request): JsonResponse
     {
-        $productList = $productRepository->findAll();
+
+        $page = $request->get('page', 1);
+        $limit = $request->get('limit', 3);
+
+        $productList = $productRepository->findAllWithPagination($page, $limit);
 
         $context = SerializationContext::create()->setGroups(['getProducts']);
         $jsonProductList = $serializer->serialize($productList, 'json', $context);
